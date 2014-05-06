74e26c3d397fefea6428ab2250ede6079ac3f661
Muhammad Miftah <s2757691@griffithuni.edu.au>
2014-05-06 19:09:52 +1000

    Created migration to add timestamp columns to the database.

---
+++ app/database/migrations/2014_05_06_190501_add_timestamps_to_tables.php
@@ -0,0 +1,72 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class AddTimestampsToTables extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('user', function(Blueprint $table) {
+			$table->timestamps();
+		});
+
+		Schema::table('deal', function(Blueprint $table) {
+			$table->timestamps();
+		});
+
+		Schema::table('product', function(Blueprint $table) {
+			$table->timestamps();
+		});
+
+		Schema::table('retailer', function(Blueprint $table) {
+			$table->timestamps();
+		});
+
+		Schema::table('shop', function(Blueprint $table) {
+			$table->timestamps();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('user', function(Blueprint $table) {
+			$table->dropColumn('created_at');
+			$table->dropColumn('updated_at');
+		});
+
+		Schema::table('deal', function(Blueprint $table) {
+			$table->dropColumn('created_at');
+			$table->dropColumn('updated_at');
+		});
+
+		Schema::table('product', function(Blueprint $table) {
+			$table->dropColumn('created_at');
+			$table->dropColumn('updated_at');
+		});
+
+		Schema::table('retailer', function(Blueprint $table) {
+			$table->dropColumn('created_at');
+			$table->dropColumn('updated_at');
+		});
+
+		Schema::table('shop', function(Blueprint $table) {
+			$table->dropColumn('created_at');
+			$table->dropColumn('updated_at');
+		});
+
+
+	}
+
+}
