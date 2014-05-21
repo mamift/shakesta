3a272b997909334f15b215f3b70eb2ae09a134a8
Muhammad Miftah <s2757691@griffithuni.edu.au>
2014-05-21 15:45:08 +1000

    Added some more comments.

--- app/controllers/APIController.php
+++ app/controllers/APIController.php
@@ -40,8 +40,9 @@ class APIController extends \BaseController {
 	 * Store a newly created resource in storage.
 	 * $fillable = ['price_discount','terms','expires_time','begins_time','category','product_id'];
 	 * test with: 
-	 * (false) curl -i --user admin:gizmoe99 -d 'price_discount=0.5&terms=blah blah' localhost:8000/api/v1.1/productdeals
-	 * (success) curl -i --user admin:gizmoe99 -d 'price_discount=0.5&terms=blah blah&expires_time=2014-05-31 05:12:00&begins_time=2014-05-01 00:00:01&category=Home goods&product_id=5' localhost:8000/api/v1.1/productdeals
+	 * (fail test 1) curl -i --user admin:gizmoe99 -d 'price_discount=0.5&terms=blah blah' localhost:8000/api/v1.1/productdeals
+	 * (fail test 2) curl -i --user admin:gizmoe99 -d 'price_discount=0.5&terms=blah blah&expires_time=2014-05-31 05:12:00&begins_time=2014-05-01 00:00:01&category=Home goods&product_id=99' localhost:8000/api/v1.1/productdeals
+	 * (success test) curl -i --user admin:gizmoe99 -d 'price_discount=0.5&terms=blah blah&expires_time=2014-05-31 05:12:00&begins_time=2014-05-01 00:00:01&category=Home goods&product_id=5' localhost:8000/api/v1.1/productdeals
 	 * @return Response
 	 */
 	public function store()
@@ -85,7 +86,7 @@ class APIController extends \BaseController {
 			if ($product_validated)
 				$status = !$deal->save();
 			else 
-				$status = false; // for now, when product validation fails, set this as false (to mean that everything's fine); we'll use the $product_validated variable to check later
+				$status = false; // for now, when product validation fails, set this as false (to mean that everything's fine); we'll use the $product_validated variable to check later. we just don't want to call $deal->save when there no valid product_id. Laravel will throw its own exception and return HTML.
 
  		} else {
 			$status = true; // we have a situation
@@ -94,10 +95,10 @@ class APIController extends \BaseController {
 		$response = array('status' => $status, 'message' => null, 'deal' => null);
 
 		$response['deal'] = [
-					'input provided was' => $input, 
-					'product_id ok?' => $product_validated,
-					'validation passed?' => $validated,
-					'validation messages' => array($validator->messages()->all())
+				'input provided was' => $input, 
+				'product_id ok?' => $product_validated,
+				'validation passed?' => $validated,
+				'validation messages' => array($validator->messages()->all())
 			];
 
 		// if we have a situation...
