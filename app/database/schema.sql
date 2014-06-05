ea19be8f24fa461f7d50b2623e493c7999687a0d
Muhammad Miftah <s2757691@griffithuni.edu.au>
2014-06-05 10:23:15 +1000

    Created clean version of schema.sql

--- app/database/schema.sql
+++ app/database/schema.sql
@@ -1,136 +1,313 @@
--- SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
--- SET time_zone = "+00:00";
+-- phpMyAdmin SQL Dump
+-- version 3.5.5
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jun 04, 2014 at 07:21 PM
+-- Server version: 5.5.33-31.1
+-- PHP Version: 5.4.23
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
-CREATE DATABASE IF NOT EXISTS `shakesta` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `shakesta`;
+--
+-- Database: `chana13_shakesta`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `admin_users`
+--
+CREATE TABLE IF NOT EXISTS `admin_users` (
+`user_id` mediumint(9)
+,`username` text
+,`password` text
+,`email` mediumtext
+,`retailer_id` mediumint(9)
+,`remember_token` varchar(100)
+,`created_at` timestamp
+,`updated_at` timestamp
+);
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE IF NOT EXISTS `category` (
+  `name` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`name`),
+  KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `deal`
+--
 
 CREATE TABLE IF NOT EXISTS `deal` (
-  `deal_id` mediumint NOT NULL AUTO_INCREMENT,
-  `price_discount` decimal(2,2),
+  `deal_id` mediumint(9) NOT NULL AUTO_INCREMENT,
+  `title` varchar(500) DEFAULT NULL,
+  `product_id` mediumint(9) DEFAULT NULL,
+  `price_discount` float DEFAULT NULL,
   `terms` text,
-  `expires_time` float DEFAULT NULL,
-  `begins_time` float DEFAULT NULL,
-  `category` tinytext,
-  PRIMARY KEY (`deal_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `expires_time` datetime DEFAULT NULL,
+  `begins_time` datetime DEFAULT NULL,
+  `category` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`deal_id`),
+  KEY `product_id` (`product_id`),
+  KEY `category` (`category`),
+  KEY `category_2` (`category`),
+  KEY `category_3` (`category`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=48 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE IF NOT EXISTS `migrations` (
+  `migration` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
 
 CREATE TABLE IF NOT EXISTS `product` (
-  `product_id` mediumint NOT NULL AUTO_INCREMENT,
+  `product_id` mediumint(9) NOT NULL AUTO_INCREMENT,
   `title` tinytext,
   `description` text,
-  `retailer_id` mediumint,
-  `retail_price` decimal(10,2),
-  `deal_id` mediumint,
-  `image` varbinary(32000),
+  `retailer_id` mediumint(9) DEFAULT NULL,
+  `retail_price` decimal(10,2) DEFAULT NULL,
+  `image` varbinary(32000) DEFAULT NULL,
+  `image_url` text,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`product_id`),
-  KEY `retailer_product_FK1` (`retailer_id`),
-  KEY `deal_product_FK1` (`deal_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  KEY `retailer_product_FK1` (`retailer_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=33 ;
+
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `product_deals`
+--
+CREATE TABLE IF NOT EXISTS `product_deals` (
+`id` mediumint(9)
+,`title` varchar(500)
+,`product_id` mediumint(9)
+,`price_discount` float
+,`terms` text
+,`expires_time` datetime
+,`begins_time` datetime
+,`category` varchar(255)
+,`created_at` timestamp
+,`updated_at` timestamp
+,`product_title` tinytext
+,`product_description` text
+,`original_price` decimal(10,2)
+,`image_url` text
+);
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `product_deals_retailers`
+--
+CREATE TABLE IF NOT EXISTS `product_deals_retailers` (
+`id` mediumint(9)
+,`product_id` mediumint(9)
+,`title` varchar(500)
+,`price_discount` float
+,`terms` text
+,`expires_time` datetime
+,`begins_time` datetime
+,`category` varchar(255)
+,`created_at` timestamp
+,`updated_at` timestamp
+,`product_title` tinytext
+,`product_description` text
+,`original_price` decimal(10,2)
+,`image_url` text
+,`retailer_id` mediumint(9)
+,`retailer` tinytext
+);
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `product_retailers`
+--
+CREATE TABLE IF NOT EXISTS `product_retailers` (
+`product_id` mediumint(9)
+,`title` tinytext
+,`description` text
+,`retailer_id` mediumint(9)
+,`retail_price` decimal(10,2)
+,`image` varbinary(32000)
+,`image_url` text
+,`created_at` timestamp
+,`updated_at` timestamp
+,`retailer` tinytext
+);
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `retailer`
+--
 
 CREATE TABLE IF NOT EXISTS `retailer` (
-  `retailer_id` mediumint NOT NULL AUTO_INCREMENT,
+  `retailer_id` mediumint(9) NOT NULL AUTO_INCREMENT,
+  `title` tinytext,
   `description` text,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`retailer_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=21 ;
+
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `retail_users`
+--
+CREATE TABLE IF NOT EXISTS `retail_users` (
+`user_id` mediumint(9)
+,`username` text
+,`email` mediumtext
+,`retailer_id` mediumint(9)
+,`retailer` tinytext
+,`retailer_description` text
+,`created_at` timestamp
+,`updated_at` timestamp
+);
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shop`
+--
 
 CREATE TABLE IF NOT EXISTS `shop` (
   `shop_location` varchar(255) NOT NULL,
-  `retailer_id` mediumint DEFAULT NULL,
+  `retailer_id` mediumint(9) DEFAULT NULL,
   `beacon_id` text,
   `title` tinytext NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`shop_location`),
   KEY `retailer_shop_FK1` (`retailer_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
 CREATE TABLE IF NOT EXISTS `user` (
-  `user_id` mediumint NOT NULL AUTO_INCREMENT,
+  `user_id` mediumint(9) NOT NULL AUTO_INCREMENT,
   `username` text,
   `password` text,
-  `retailer_id` mediumint DEFAULT NULL,
+  `email` mediumtext,
+  `notes` text,
+  `retailer_id` mediumint(9) DEFAULT NULL,
+  `remember_token` varchar(100) DEFAULT NULL,
+  `apikey` varchar(34) DEFAULT NULL,
+  `status` enum('enabled','disabled') DEFAULT 'disabled',
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`user_id`),
-  KEY `retailer_user_FK1` (`retailer_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  UNIQUE KEY `apikey` (`apikey`),
+  KEY `retailer_user_FK1` (`retailer_id`),
+  KEY `apikey_2` (`apikey`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=56 ;
 
+-- --------------------------------------------------------
 
+--
+-- Structure for view `admin_users`
+--
+DROP TABLE IF EXISTS `admin_users`;
+
+CREATE OR REPLACE VIEW `admin_users` AS (select `user`.`user_id` AS `user_id`,`user`.`username` AS `username`,`user`.`password` AS `password`,`user`.`email` AS `email`,`user`.`retailer_id` AS `retailer_id`,`user`.`remember_token` AS `remember_token`,`user`.`created_at` AS `created_at`,`user`.`updated_at` AS `updated_at` from `user` where isnull(`user`.`retailer_id`));
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `product_deals`
+--
+DROP TABLE IF EXISTS `product_deals`;
+
+CREATE OR REPLACE VIEW `product_deals` AS select `deal`.`deal_id` AS `id`,`deal`.`title` AS `title`,`deal`.`product_id` AS `product_id`,`deal`.`price_discount` AS `price_discount`,`deal`.`terms` AS `terms`,`deal`.`expires_time` AS `expires_time`,`deal`.`begins_time` AS `begins_time`,`deal`.`category` AS `category`,`deal`.`created_at` AS `created_at`,`deal`.`updated_at` AS `updated_at`,`product`.`title` AS `product_title`,`product`.`description` AS `product_description`,`product`.`retail_price` AS `original_price`,`product`.`image_url` AS `image_url` from (`deal` join `product` on((`product`.`product_id` = `deal`.`product_id`)));
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `product_deals_retailers`
+--
+DROP TABLE IF EXISTS `product_deals_retailers`;
+
+CREATE OR REPLACE VIEW `product_deals_retailers` AS select `deal`.`deal_id` AS `id`,`deal`.`product_id` AS `product_id`,`deal`.`title` AS `title`,`deal`.`price_discount` AS `price_discount`,`deal`.`terms` AS `terms`,`deal`.`expires_time` AS `expires_time`,`deal`.`begins_time` AS `begins_time`,`deal`.`category` AS `category`,`deal`.`created_at` AS `created_at`,`deal`.`updated_at` AS `updated_at`,`product`.`title` AS `product_title`,`product`.`description` AS `product_description`,`product`.`retail_price` AS `original_price`,`product`.`image_url` AS `image_url`,`product`.`retailer_id` AS `retailer_id`,`retailer`.`title` AS `retailer` from ((`deal` join `product` on((`product`.`product_id` = `deal`.`product_id`))) join `retailer` on((`product`.`retailer_id` = `retailer`.`retailer_id`)));
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `product_retailers`
+--
+DROP TABLE IF EXISTS `product_retailers`;
+
+CREATE OR REPLACE VIEW `product_retailers` AS select `product`.`product_id` AS `product_id`,`product`.`title` AS `title`,`product`.`description` AS `description`,`product`.`retailer_id` AS `retailer_id`,`product`.`retail_price` AS `retail_price`,`product`.`image` AS `image`,`product`.`image_url` AS `image_url`,`product`.`created_at` AS `created_at`,`product`.`updated_at` AS `updated_at`,`retailer`.`title` AS `retailer` from (`product` join `retailer` on((`product`.`retailer_id` = `retailer`.`retailer_id`)));
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `retail_users`
+--
+DROP TABLE IF EXISTS `retail_users`;
+
+CREATE OR REPLACE VIEW `retail_users` AS select distinct `user`.`user_id` AS `user_id`,`user`.`username` AS `username`,`user`.`email` AS `email`,`user`.`retailer_id` AS `retailer_id`,`retailer`.`title` AS `retailer`,`retailer`.`description` AS `retailer_description`,`user`.`created_at` AS `created_at`,`user`.`updated_at` AS `updated_at` from (`user` `users_table` join (`retailer` join `user` on((`user`.`retailer_id` = `retailer`.`retailer_id`))));
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `deal`
+--
+ALTER TABLE `deal`
+  ADD CONSTRAINT `deal_ibfk_2` FOREIGN KEY (`category`) REFERENCES `category` (`name`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `deal_product_FK1` FOREIGN KEY (`product_id`) REFERENCES `product` (`product_id`);
+
+--
+-- Constraints for table `product`
+--
 ALTER TABLE `product`
-  ADD CONSTRAINT `deal_product_FK1` FOREIGN KEY (`deal_id`) REFERENCES `deal` (`deal_id`),
   ADD CONSTRAINT `retailer_product_FK1` FOREIGN KEY (`retailer_id`) REFERENCES `retailer` (`retailer_id`);
 
+--
+-- Constraints for table `shop`
+--
 ALTER TABLE `shop`
   ADD CONSTRAINT `retailer_shop_FK1` FOREIGN KEY (`retailer_id`) REFERENCES `retailer` (`retailer_id`);
 
+--
+-- Constraints for table `user`
+--
 ALTER TABLE `user`
   ADD CONSTRAINT `retailer_user_FK1` FOREIGN KEY (`retailer_id`) REFERENCES `retailer` (`retailer_id`);
-
-ALTER TABLE `deal` CHANGE `expires_time` `expires_time` DATETIME NULL DEFAULT NULL;
-ALTER TABLE `deal` CHANGE `begins_time` `begins_time` DATETIME NULL DEFAULT NULL;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-/* Actually the constraint here is bad, the deal table must have a fk to product, not the other way around */
-ALTER TABLE `product` DROP FOREIGN KEY `product`.`deal_product_FK1`;
-ALTER TABLE `product` DROP `deal_id`;
-ALTER TABLE `deal` ADD `product_id` MEDIUMINT NULL AFTER `deal_id`, ADD UNIQUE (`product_id`);
-ALTER TABLE `deal` ADD CONSTRAINT `deal_product_FK1` FOREIGN KEY (`product_id`) REFERENCES `product` (`product_id`);
-
-ALTER TABLE `retailer` ADD `title` TINYTEXT NULL AFTER `retailer_id`;
-
-/* Default user */
-INSERT INTO `shakesta`.`user` (`user_id`, `username`, `password`, `retailer_id`, `created_at`, `updated_at`) VALUES ('1', 'admin', '$2y$10$G52UpoIxmzf7ZkOgvP4T6eX6hdm2SuXnHUpV67GwCBRbR6zfWr2nK', NULL, '0000-00-00 00:00:00', '0000-00-00 00:00:00');
-
-ALTER TABLE `user` ADD `email` MEDIUMTEXT NULL AFTER `password`;
-ALTER TABLE `product` ADD `image_url` TEXT NULL AFTER `image`;
-
-ALTER TABLE `shakesta`.`deal` DROP INDEX `product_id`, ADD INDEX `product_id` (`product_id`)COMMENT '';
-
-/* http://laravel.com/docs/security#configuration */
-ALTER TABLE `user` ADD `remember_token` VARCHAR(100) NULL AFTER `retailer_id`;
-
-ALTER TABLE `user` ADD `apikey` VARCHAR(34) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL AFTER `remember_token`, ADD UNIQUE (`apikey`) ;
-ALTER TABLE `user` ADD INDEX(`apikey`);
-
-/* either enabled or disabled */
-ALTER TABLE `user` ADD `status` VARCHAR(10) NULL DEFAULT NULL AFTER `apikey`;
-ALTER TABLE `user` CHANGE `status` `status` ENUM('enabled','disabled') CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT 'disabled';
-
-
-/* categories table */
-CREATE TABLE IF NOT EXISTS `category` (
-  `name` varchar(255) NOT NULL DEFAULT '',
-  PRIMARY KEY (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-/* default categories */
-INSERT INTO `chana13_shakesta`.`category` (`name`) VALUES ('Electrical & Electronics');
-INSERT INTO `chana13_shakesta`.`category` (`name`) VALUES ('Food & Grocery');
-INSERT INTO `chana13_shakesta`.`category` (`name`) VALUES ('Computing');
-INSERT INTO `chana13_shakesta`.`category` (`name`) VALUES ('Gaming');
-INSERT INTO `chana13_shakesta`.`category` (`name`) VALUES ('Fashion & Apparel');
-INSERT INTO `chana13_shakesta`.`category` (`name`) VALUES ('Mobile');
-INSERT INTO `chana13_shakesta`.`category` (`name`) VALUES ('Entertainment');
-INSERT INTO `chana13_shakesta`.`category` (`name`) VALUES ('Travel');
-
-/* we're now enabling linked categories */
-ALTER TABLE `deal` CHANGE `category` `category` VARCHAR(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL;
-ALTER TABLE `deal` ADD INDEX(`category`);
-
-ALTER TABLE `deal` ADD CONSTRAINT `deal_categories` FOREIGN KEY (`category`) REFERENCES `chana13_shakesta`.`category`(`name`) ON DELETE RESTRICT ON UPDATE CASCADE;
-
-/** need to allow 1.0 in price_discount changes **/
-ALTER TABLE `deal` CHANGE `price_discount` `price_discount` FLOAT NULL DEFAULT NULL;
-
-/* minor user detail: notes for self-registrered users */
-ALTER TABLE `user` ADD `notes` TEXT NULL DEFAULT NULL AFTER `email`;
-
-/* added title field for deals */
-ALTER TABLE `deal` ADD `title` VARCHAR(500) NULL DEFAULT NULL AFTER `deal_id`;
-
-
-
